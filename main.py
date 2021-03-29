761cc8e750150d0e0fb891133067a44c50332b0b
Zekeriya AY <ay.zekeriya2000@gmail.com>
2021-03-30 00:17:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ TrixBot Yardım İsteği
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='?', description=description, intents=intents)
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logging.basicConfig(level=logging.DEBUG)
 
 cogs = ['cogs.owner', 'cogs.exception', 'cogs.event', 'cogs.moderation']
 for cog in cogs:
@@ -26,13 +21,11 @@ for cog in cogs:
 async def sa(ctx):
     await ctx.send("as")
 
-
 @client.event
 async def on_ready():
     print(f'{client.user} çalışıyor...')
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name="discord.io/ERYSTRIX"))
-
 
 @client.event
 async def on_message(message):
